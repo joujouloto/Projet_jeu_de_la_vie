8faa23a844a7b752eafa69c35f92b2c400a262f3
joujouloto <166531433+joujouloto@users.noreply.github.com>
2025-02-10 17:22:09 +0100

    Libere memoire
    libere memoire

--- src/main_sdl.cpp
+++ src/main_sdl.cpp
@@ -10,6 +10,41 @@
 #include<thread>
 
 
+//Images
+		
+SDL_Surface * image_vide = SDL_LoadBMP("../../images/vide.bmp");
+	
+SDL_Surface * image_arbre = SDL_LoadBMP("../../images/arbre.bmp");
+	
+SDL_Surface * image_gaulois = SDL_LoadBMP("../../images/gaulois.bmp");
+	
+SDL_Surface * image_gauloise = SDL_LoadBMP("../../images/gauloise.bmp");
+	
+SDL_Surface * image_animal = SDL_LoadBMP("../../images/animal.bmp");
+
+SDL_Surface * image_gaulois_enfant_garcon =
+	 SDL_LoadBMP("../../images/gaulois_enfant_garcon.bmp");
+
+SDL_Surface * image_gaulois_enfant_fille =
+	 SDL_LoadBMP("../../images/gaulois_enfant_fille.bmp");
+	 
+	 
+	 
+SDL_Texture * texture_vide = NULL;
+
+SDL_Texture * texture_arbre = NULL;
+
+SDL_Texture * texture_gaulois = NULL;
+
+SDL_Texture * texture_gauloise = NULL;
+
+SDL_Texture * texture_animal = NULL;
+
+SDL_Texture * texture_gaulois_garcon = NULL;
+
+SDL_Texture * texture_gaulois_fille = NULL;
+
+
 void main_sdl()
 {
     SDL_Window *win;
@@ -107,43 +142,22 @@ void afficher_grille_SDL(SDL_Renderer *renderer, _grille grille )
 	single_rect.w = 50;
 	single_rect.h = 50;
 	
-	//Images
-		
-	SDL_Surface * image_vide = SDL_LoadBMP("../../images/vide.bmp");
-	
-	SDL_Surface * image_arbre = SDL_LoadBMP("../../images/arbre.bmp");
-	
-	SDL_Surface * image_gaulois = SDL_LoadBMP("../../images/gaulois.bmp");
-	
-	SDL_Surface * image_gauloise = SDL_LoadBMP("../../images/gauloise.bmp");
-	
-	SDL_Surface * image_animal = SDL_LoadBMP("../../images/animal.bmp");
-
-	SDL_Surface * image_gaulois_enfant_garcon =
-	  SDL_LoadBMP("../../images/gaulois_enfant_garcon.bmp");
-
-	SDL_Surface * image_gaulois_enfant_fille =
-	  SDL_LoadBMP("../../images/gaulois_enfant_fille.bmp");
-	
-		
-	//Textures
-	SDL_Texture * texture_vide = SDL_CreateTextureFromSurface(renderer, image_vide);
-	
-	SDL_Texture * texture_arbre = SDL_CreateTextureFromSurface(renderer, image_arbre);
-	
-	SDL_Texture * texture_gaulois = SDL_CreateTextureFromSurface(renderer, image_gaulois);
-	
-	SDL_Texture * texture_gauloise = SDL_CreateTextureFromSurface(renderer, image_gauloise);
-	
-	SDL_Texture * texture_animal = SDL_CreateTextureFromSurface(renderer, image_animal);
-
-	SDL_Texture * texture_gaulois_garcon = SDL_CreateTextureFromSurface(renderer, image_gaulois_enfant_garcon);
-
-	SDL_Texture * texture_gaulois_fille = SDL_CreateTextureFromSurface(renderer, image_gaulois_enfant_fille);
-	
 	//169*221
 	
-	
+	//Textures
+	texture_vide = SDL_CreateTextureFromSurface(renderer, image_vide);
+
+	texture_arbre = SDL_CreateTextureFromSurface(renderer, image_arbre);
+
+	texture_gaulois = SDL_CreateTextureFromSurface(renderer, image_gaulois);
+
+	texture_gauloise = SDL_CreateTextureFromSurface(renderer, image_gauloise);
+
+	texture_animal = SDL_CreateTextureFromSurface(renderer, image_animal);
+
+	texture_gaulois_garcon = SDL_CreateTextureFromSurface(renderer, image_gaulois_enfant_garcon);
+
+	texture_gaulois_fille = SDL_CreateTextureFromSurface(renderer, image_gaulois_enfant_fille);
 	
 	int pos_x = 40;
 	int pos_y = 40;
@@ -218,6 +232,39 @@ void afficher_grille_SDL(SDL_Renderer *renderer, _grille grille )
 
 void fermeture_SDL(SDL_Window *win, SDL_Renderer *renderer)
 {
+	
+	//free textures
+	SDL_DestroyTexture(texture_vide);
+	
+	SDL_DestroyTexture(texture_arbre);
+	
+	SDL_DestroyTexture(texture_gaulois);
+	
+	SDL_DestroyTexture(texture_gauloise);
+	
+	SDL_DestroyTexture(texture_animal);
+	
+	SDL_DestroyTexture(texture_gaulois_garcon);
+	
+	SDL_DestroyTexture(texture_gaulois_fille);
+	
+	//free surfaces
+	
+	SDL_FreeSurface(image_vide);
+	
+	SDL_FreeSurface(image_arbre);
+	
+	SDL_FreeSurface(image_gaulois);
+	
+	SDL_FreeSurface(image_gauloise);
+	
+	SDL_FreeSurface(image_animal);
+	
+	SDL_FreeSurface(image_gaulois_enfant_garcon);
+	
+	SDL_FreeSurface(image_gaulois_enfant_fille);
+	
+	
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
